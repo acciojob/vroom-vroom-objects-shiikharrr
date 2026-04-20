aa85aba57fd7cc846547d6753fea8fc49b812623
Shikhar Gupta <69209995+shiikharrr@users.noreply.github.com>
2026-04-20 12:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Car(make, model) {
 }
 
 Car.prototype.getMakeModel = function (){
-	return `$(this.make) $(this.model)`;
+	return `${this.make} ${this.model}`;
 };
 
 function SportsCar(make, model, topSpeed) {
